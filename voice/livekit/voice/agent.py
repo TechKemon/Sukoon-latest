f9c13311a73facad63347bc0ad8db3c634c7a790
LUV SINGH <luv@peopleplus.ai>
2025-02-17 06:47:09 +0530

    added voice files

---
+++ voice/livekit/voice/agent.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import logging
+from dotenv import load_dotenv
+
+from livekit import rtc
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.multimodal import MultimodalAgent
+from livekit.plugins import openai
+
+
+load_dotenv(dotenv_path=".env.local")
+logger = logging.getLogger("my-worker")
+logger.setLevel(logging.INFO)
+
+
+async def entrypoint(ctx: JobContext):
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    participant = await ctx.wait_for_participant()
+
+    run_multimodal_agent(ctx, participant)
+
+    logger.info("agent started")
+
+
+def run_multimodal_agent(ctx: JobContext, participant: rtc.RemoteParticipant):
+    logger.info("starting multimodal agent")
+
+    model = openai.realtime.RealtimeModel(
+        instructions=(
+            "Help users with theie mental health condition "
+            "You should use short and concise responses, and try to detect emotion from voice "
+            "be emphathetic at all times"
+        ),
+        modalities=["audio", "text"],
+    )
+
+    # create a chat context with chat history, these will be synchronized with the server
+    # upon session establishment
+    chat_ctx = llm.ChatContext()
+    chat_ctx.append(
+        text="Context about the user: you are talking to a software engineer who's building voice AI applications."
+        "Greet the user with a friendly greeting and ask how you can help them today.",
+        role="assistant",
+    )
+
+    agent = MultimodalAgent(
+        model=model,
+        chat_ctx=chat_ctx,
+    )
+    agent.start(ctx.room, participant)
+
+    # to enable the agent to speak first
+    agent.generate_reply()
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+        )
+    )
